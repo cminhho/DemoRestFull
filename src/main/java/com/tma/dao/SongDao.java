ba9916e08f914674a4a3929986768054bc0d43dd
cminhho <hmchung92@gmail.com>
2015-01-30 22:51:31 +0700

    delete src

--- src/main/java/com/tma/dao/SongDao.java
+++
@@ -1,9 +0,0 @@
-package com.tma.dao;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.tma.model.Song;
-
-public interface SongDao extends MongoRepository<Song, String> {
-
-}
